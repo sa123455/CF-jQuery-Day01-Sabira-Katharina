dedf0bc5706ae7bb9bf58e93a3c8abd0bf2469af
Sabira Aliu <sabiraaliu@gmail.com>
2020-10-06 12:48:26 +0200

    sth.

--- photoAlbum/script.js
+++ photoAlbum/script.js
@@ -3,11 +3,13 @@ console.table(familyAlbum);
 
 
 
-$(document).ready(function(){
+$(document).ready(function() {
 
 
 
-    for (i=0; i<familyAlbum.length; i++){
+    for (i = 0; i < familyAlbum.length; i++) {
+        //<div src= familyPhoto></div>
+
         var familyPhoto = familyAlbum[i].img;
         //console.log(familyPhoto);
         $("#mainDiv").append(familyPhoto);
@@ -21,4 +23,3 @@ $(document).ready(function(){
 
 
 })
-
